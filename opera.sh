a258b96d41723bac13e46971711cbf7bc98aaa31
Frank Oldfield <48333349+frankoldfield@users.noreply.github.com>
2022-10-04 19:22:12 +0200

    Create opera.sh
    ejercicio 2 boletin 3 shell

---
+++ opera.sh
@@ -0,0 +1,8 @@
+let sum=$1 + $2
+let res=$1 - $2
+let prod=$1 * $2
+let div=$1/$2
+echo "La suma de $1 más $2 es $sum"
+echo "La resta de $1 menos $2 es $res"
+echo "El producto de $1 y $2 es $prod"
+echo "La division de $1 entre $2 es $div"
