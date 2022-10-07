de655571f0aafc8274c4fa7e5b07dfe9c41a83fd
Frank Oldfield <48333349+frankoldfield@users.noreply.github.com>
2022-10-07 14:00:42 +0200

    opcion con orden compuesta
    para que la salida salga bien ordenada

--- 5.totalvsz.sh
+++ 5.totalvsz.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-lista=$(ps aux | tail -n+2 | tr -s ' ' | cut -f1 -d ' ' | sort | uniq)
-
-echo "$lista" | while read usuario
+(echo USER VSZ;
+ps aux | tail -n+2 | tr -s ' ' | cut -f1 -d ' ' | sort | uniq | while read usuario
 do
 	tamano=$(ps aux | tail -n+2 | grep "^$usuario" | tr -s ' ' | cut -f5 -d' ' | paste -s -d'+' | bc)
 	echo $usuario $tamano
-done
+done) | column -t
 
