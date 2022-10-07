4095853d512553ead0070ecfa85ec60d7ae716e5
Frank Oldfield <48333349+frankoldfield@users.noreply.github.com>
2022-10-07 19:58:08 +0200

    Script funcional
    El error era que el asterisco tiene que ir sin espacios

--- doble_interactivo.sh
+++ doble_interactivo.sh
@@ -1,14 +1,15 @@
+#!/bin/bash
 opcion="s"
 while test "$opcion" = "s"
 do
 	echo "Introduce un número para calcular el doble:"
 	read num
-	let $doble=$num * 2
-	echo "El doble de $num es $dob"
+	let doble=$num*2
+	echo "El doble de $num es $doble"
 	echo "¿Quiere calcular otro doble?"
 	read opcion
 done
-if $opcion != n
+if test $opcion != n
 then
 	exit 1
 else
