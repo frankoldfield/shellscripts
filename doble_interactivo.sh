0d8428b56ed03e87cde7b322e7d49385902b0d16
Frank Oldfield <48333349+frankoldfield@users.noreply.github.com>
2022-10-07 13:07:50 +0200

    Ejercicio 5 práctica 3

---
+++ doble_interactivo.sh
@@ -0,0 +1,16 @@
+opcion="s"
+while test "$opcion" = "s"
+do
+	echo "Introduce un número para calcular el doble:"
+	read num
+	let $doble=$num * 2
+	echo "El doble de $num es $dob"
+	echo "¿Quiere calcular otro doble?"
+	read opcion
+done
+if $opcion != n
+then
+	exit 1
+else
+	exit 0
+fi
